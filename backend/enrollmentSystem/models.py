189c436c38bdfdeabe99f9c6bb2a047637e58c23
farmerzhang1 <zhang2000dc@gmail.com>
2020-06-12 10:23:14 +0800

    add grade

--- backend/enrollmentSystem/models.py
+++ backend/enrollmentSystem/models.py
@@ -3,11 +3,36 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=64, null=True)
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    GRADE = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    grade = models.CharField(max_length = 2, choices=GRADE, default = FRESHMAN)
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.user.username
 class Course(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    GRADE = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    grade = models.CharField(max_length = 2, choices=GRADE, default = FRESHMAN)
     name = models.CharField(max_length=255, null=True)
     description = models.CharField(max_length=1000, null = True)
     student = models.ManyToManyField(Student)
