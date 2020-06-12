fc64ff5df28d2cc7bb7ceb9653ccffad087a4aeb
totoroyyw <953834564@qq.com>
2020-06-12 22:52:15 +0800

    jw update urls due to README

--- backend/jw/urls.py
+++ backend/jw/urls.py
@@ -4,5 +4,8 @@ from jw import views
 
 urlpatterns = [
     path('getall/', views.GetAllView.as_view(), name='getall_view'),
+    path('getprivke/', views.GetPrivkeView.as_view(), name='getprivke_view'),
+    path('elecke/', views.ElecKeView.as_view(), name='elecke_view'),
+    path('dropke/', views.DropKeView.as_view(), name='dropke_view'),
 ]
 
