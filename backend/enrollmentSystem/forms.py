8cb6ac43c51da8fc567c27c8b6e428d9c9e497b1
farmerzhang1 <zhang2000dc@gmail.com>
2020-06-15 09:09:04 +0800

    add grade to UserForm

--- backend/enrollmentSystem/forms.py
+++ backend/enrollmentSystem/forms.py
@@ -3,6 +3,19 @@ from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 class UserForm(UserCreationForm):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    GRADE = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    grade = models.CharField(max_length = 2, choices=GRADE, default = FRESHMAN)
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'grade', 'password1', 'password2']
