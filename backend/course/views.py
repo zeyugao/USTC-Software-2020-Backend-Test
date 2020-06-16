95ae5010b956bc8da466baf5f06c0de3de1148f2
farmerzhang1 <zhang2000dc@gmail.com>
2020-06-16 23:20:27 +0800

    changed to class-based view

--- backend/course/views.py
+++ backend/course/views.py
@@ -3,62 +3,73 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .models import Course
 from account.models import Student
+from django.views import View
+from django.utils.decorators import method_decorator
 # Create your views here.
-@login_required
-def enroll(request, pk):
-    try:
-        course = Course.objects.get(pk=pk)
-    except Course.DoesNotExist:
-        return JsonResponse({
-            'status': 404,
-            'msg': 'Course not found'
-        })
-    context = { 'course': course }
-    if request.method == 'POST':
-        student = request.user.student
-        course.student.add(student)
+
+@method_decorator(login_required, name = 'dispatch')
+class Enroll(View):
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        try:
+            course = Course.objects.get(pk = pk)
+        except Course.DoesNotExist:
+            return JsonResponse({
+                'status': 404,
+                'msg': 'Course not found'
+            })
+        context = { 'course': course }
+        if request.method == 'POST':
+            student = request.user.student
+            course.student.add(student)
+            return JsonResponse({
+                'status': 200,
+                'msg': 'Added Successfully'
+            })
+
+class AllCourses(View):
+    def get(self, request, *args, **kwargs):
+        queryset = Course.objects.all()
+        context = { 'queryset': queryset }
         return JsonResponse({
             'status': 200,
-            'msg': 'Added Successfully'
+            'msg': 'Success'
         })
 
-def allCourses(request):
-    queryset = Course.objects.all()
-    context = { 'queryset': queryset }
-    return JsonResponse({
-        'status': 200,
-        'msg': 'Success'
-    })
-
-@login_required
-def availableCourses(request):
-    my_grade = request.user.student.grade
-    queryset = Course.objects.filter(grade=my_grade)
-    context = { 'queryset': queryset }
-    return JsonResponse({
-        'status': 200,
-        'msg': 'Success'
-    })
-
-@login_required
-def myCourse(request):
-    courses = request.user.student.course_set.all()
-    context = { 'courses': courses }
-    return JsonResponse({
-        'status': 200,
-        'msg': 'Success'
-    })
-
-@login_required
-def dropCourse(request, pk):
-    try:
-        course = Course.objects.get(pk=pk)
-    except Course.DoesNotExist:
+@method_decorator(login_required, name = 'dispatch')
+class AvailableCourses(View):
+    def get(self, request, *args, **kwargs):
+        my_grade = request.user.student.grade
+        queryset = Course.objects.filter(grade=my_grade)
+        context = { 'queryset': queryset }
         return JsonResponse({
-            'status': 404,
-            'msg': 'Course not found'
+            'status': 200,
+            'msg': 'Success'
         })
-    if request.method == 'POST':
+
+@login_required
+class MyCourse(View):
+    def get(self, request, *args, **kwargs):
+        courses = request.user.student.course_set.all()
+        context = { 'courses': courses }
+        return JsonResponse({
+            'status': 200,
+            'msg': 'Success'
+        })
+
+@method_decorator(login_required, name = 'dispatch')
+class DropCourse(View):
+    def get(self, request, *args, **kwargs):
+        pass
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        try:
+            course = Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            return JsonResponse({
+                'status': 404,
+                'msg': 'Course not found'
+            })
         request.user.student.course_set.remove(course)
         return JsonResponse({
             'status': 200,
