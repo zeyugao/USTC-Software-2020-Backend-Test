75d7c5cb5cc76de8507aa479581b876cc58a716a
farmerzhang1 <zhang2000dc@gmail.com>
2020-06-16 23:20:13 +0800

    changed to class-based view

--- backend/account/views.py
+++ backend/account/views.py
@@ -5,11 +5,13 @@ from .models import *
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.views import View
 # Create your views here.
 
-def loginPage(request):
-    context = {}
-    if request.method == 'POST':
+class Login(View):
+    def get(self, request):
+        pass
+    def post(self, request, *args, **kwargs):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
@@ -25,9 +27,11 @@ def loginPage(request):
                 'msg': 'Login Failed'
             })
 
-def registerPage(request):
-    if request.method == 'POST':
-        context = {
+class Register(View):
+    def get(self, request):
+        pass
+    def post(self, request, *args, **kwargs):
+        grade_dict = {
             'Freshman': 'FR',
             'Sophmore': 'SO',
             'Junior': 'JR',
@@ -41,26 +45,26 @@ def registerPage(request):
             validate(username)
         except ValidationError as e:
             return JsonResponse({
-                'status': 410, 
-                'msg': e.message
+                'status': 410,
+                'msg': e.messages
             })
         try:#TO DO!!
             validate(password1)
         except ValidationError as e:
             return JsonResponse({
-                'status': 410, 
-                'msg': e.message
+                'status': 410,
+                'msg': e.messages
             })
-        try: 
-            grade = context[request.POST.get('grade')]
+        try:
+            grade = grade_dict[request.POST.get('grade')]
         except KeyError:
             return JsonResponse({
-                'status': 410, 
+                'status': 410,
                 'msg': 'Key does not exist'
             })
         user = User.objects.create(username=username, password=password1)
         Student.objects.create(
-            user = user, 
+            user = user,
             grade = grade
         )
         return JsonResponse({
@@ -68,10 +72,11 @@ def registerPage(request):
             'msg': 'Student Created Successfully'
         })
 
-def logoutPage(request):
-    if request.user.is_authenticated:
-        logout(request)
-    return JsonResponse({
-        'status': 200,
-        'msg': 'Logout Successfully'
-    })
+class Logout(View):
+    def post(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+        return JsonResponse({
+            'status': 200,
+            'msg': 'Logout Successfully'
+        })
